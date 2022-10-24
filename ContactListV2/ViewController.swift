d9dae2d9828e4f3cf835ab6214958a576b9b4a96
Albina <albina.exe@mail.ru>
2022-10-24 16:52:57 +0200

    Initial Commit

---
+++ ContactListV2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactListV2
+//
+//  Created by Альбина Лега on 24/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
